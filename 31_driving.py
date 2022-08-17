fcef1eca3c39a8be2c019b0f54672b8844ba95d9
Lawliet Lin <gamer99122@hotmail.com.tw>
2022-08-17 21:25:20 +0800

    新增年齡判斷，更新README

---
+++ 31_driving.py
@@ -0,0 +1,14 @@
+country=input('請問你是哪國人:')
+age=input('你的年齡:')
+age=int(age)
+if country == '台灣':
+    if age >= 18:
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照')
+elif country == '美國':
+    if age >= 18:
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照') 
+
