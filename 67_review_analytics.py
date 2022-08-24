3aeb1c6fc2be925ea6b6394c85c9225484efe029
Lawliet Lin <gamer99122@hotmail.com.tw>
2022-08-24 21:32:41 +0800

    分析文字_進階

---
+++ 67_review_analytics.py
@@ -0,0 +1,35 @@
+# 從44延伸
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 10000 == 0:
+            print(len(data))
+print('檔案讀取完了，總共有[', len(data), ']筆資料')
+
+print(data[0])
+
+wc = {}
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 100000:
+        print(word, wc[word])
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
